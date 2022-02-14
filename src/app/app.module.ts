d9302b8f2256f1c19da896671fca494d3cf5a158
Juliet <agutu@gmail.com>
2022-02-14 12:18:08 +0300

    input users profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { FormComponent } from './form/form.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GitSearchComponent,
-    FormComponent
+    FormComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
