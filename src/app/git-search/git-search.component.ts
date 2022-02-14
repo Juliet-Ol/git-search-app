8776a7c970352f8cb4b0ccbe1754d9e792d5df80
Juliet <agutu@gmail.com>
2022-02-14 19:26:38 +0300

    put the form in a container for styling

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { GitSearch } from '../git-search/git-search'
+import { GitService } from '../git-services/git.service';
 
 @Component({
   selector: 'app-git-search',
   templateUrl: './git-search.component.html',
-  styleUrls: ['./git-search.component.css']
+  styleUrls: ['./git-search.component.css'],
+  providers: [GitService]
 })
 export class GitSearchComponent implements OnInit {
 
-  constructor() { }
+  gitsearch!: GitSearch[];
+
+  constructor(gitService:GitService) { 
+    this.gitsearch = gitService.getGitHubRepo
+  }
 
   ngOnInit(): void {
   }
