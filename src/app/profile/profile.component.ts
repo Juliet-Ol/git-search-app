07e16d15c3de935aeb61c6ee5c3790f5c3d53ba2
Juliet <agutu@gmail.com>
2022-02-15 09:08:23 +0300

    input profile input details

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GitSearchComponent } from '../git-search/git-search.component';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  gitsearch!:GitSearchComponent;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
