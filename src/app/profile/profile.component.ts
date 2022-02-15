b3d19eb61f0ec56d3e923ab57e3be365bf95584e
Juliet <agutu@gmail.com>
2022-02-15 22:21:47 +0300

    add repo links

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,17 +13,36 @@ export class ProfileComponent implements OnInit {
   gitsearch!:GitSearchComponent;
   user!: Git;
   data: any;
+  repos:any = [];
+  userName:string = 'Juliet-Ol'
 
   constructor(public gitservices: GitService) {}
 
 
   ngOnInit(): void {
     console.log("hello world");
-    this.gitservices.getProfile().then(data => {
+    this.gitservices.getProfile(this.userName).then(data => {
       this.data =  data;
     });
+
+    this.gitservices.getRepos(this.userName).then(data => {
+      this.repos = data;
+      console.log(data)
+      
+    });
   }
-  
+  searchGitHubUser(){
+    this.gitservices.getProfile(this.userName).then(data => {
+      this.data =  data;
+    });
+
+    this.gitservices.getRepos(this.userName).then(data => {
+      this.repos = data;
+      console.log(data)
+      
+    });
+
+  }
 
 }
 
